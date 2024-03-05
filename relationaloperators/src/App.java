4204b5fffcbfd50c98a7a3ebe76e80c69c6c8d7e
sergioblanco <>
2024-03-05 15:41:15 -0500

    operadores relacionales y condicionales

---
+++ relationaloperators/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Integer num1 = 127;
+        Integer num2 = 127;
+
+        boolean check1 = (num1 == num2);
+
+        System.out.println(check1);
+
+        Integer num3 = 200;
+        Integer num4 = 200;
+
+        boolean check2 = (num3 == num4);
+
+        System.out.println(check2);
+    }
+}
