6fcefab833d63a1f4c68ccae145ac6700d72ebbe
SergioBlancoFtns <142640911+SergioBlancoFtns@users.noreply.github.com>
2024-03-05 10:35:01 -0500

    uso clase Cat para output en consola

--- instancevariableinjava/src/App.java
+++ instancevariableinjava/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cat mirringo = new Cat("mirringo", 4);
+
+        mirringo.displayInfo();
+
+        mirringo.setAge(2);
+
+        mirringo.displayInfo();
     }
 }
